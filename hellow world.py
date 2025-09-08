3bcc79e856a399505e0ceb09b2e019a830ce4032
SHLEE972 <seungholee972@gmail.com>
2025-09-08 15:04:58 +0900

    Update hellow world.py

--- hellow world.py
+++ hellow world.py
@@ -1,7 +1,11 @@
 def print_hello():
     print("Hello, World!")  
 
+def print_KIT():
+    print("Kumoh National Institute of Technology.")
+
 if __name__ == "__main__":
+    print_KIT()
     print_hello()
 
 # This script prints "Hello, World!" to the console.
